a176d6814eb85004d1e4d520b5cd6fa2203412ee
Neel Raiyani <nraiyani@gmail.com>
2026-02-26 20:36:07 -0500

    Simplest use of SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 import std;
 
+import sdl;
+
 auto main() -> int
 {
 	std::println("Hello World!");
+
+	auto sdl_o = sdl::sdl_base();
 
 	return 0;
 }
---
+++ src/pch.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+// #include "pch_std.hpp"
+
+#include <cassert>
+
+#include <SDL3/SDL.h> // SDL header
